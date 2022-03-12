dcc10ef96ca976969849fafe03881d8a40034bfb
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-03-12 23:31:01 +0100

    Highlight Pop remake into function component v 0.1

--- frontend/src/components/HighlightPopRemake/HighlightPop.ts
+++ frontend/src/components/HighlightPopRemake/HighlightPop.ts
@@ -1,16 +1,33 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState, useRef, useEffect } from 'react'
 import { HPopover, HPopoverItem } from './HighlightPopRemake.styled'
 
 
 interface HighlightPopProps {
 
-
-
     children: ReactNode,
 }
-}
+
 
 const HighlightPop: React.FC<HighlightPopProps> = ({ }) => {
-    return (<div></div>)
+
+
+    const [xPosition, setXPosition] = useState<number | null>(null)
+    const [yPosition, setYPosition] = useState<number | null>(null)
+    const [selectedText, setSelectedText] = useState<string>('')
+
+    return (<><div ref= { this.highlight } >
+        { showPopover && (
+            <HPopover
+            style={ { left: `${x}px`, top: `${y}px` } }
+    role = 'presentation'
+    onMouseDown = {(e) => e.preventDefault()}
+          >
+    <HPopoverItem role='button' > Слава Україні! < /HPopoverItem>
+        < HPopoverItem role = 'button' > Героям слава! < /HPopoverItem>
+            < HPopoverItem role = 'button' > Hague is waiting < /HPopoverItem>
+                < /HPopover>
+        )}
+{ children }
+</div></ >)
 }
 export default HighlightPop
