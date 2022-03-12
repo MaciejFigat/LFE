32c43c33b37513c7f85aaa6db157aa7a28fb41b6
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-03-12 21:51:24 +0100

    Highlight Pop remake into function component

---
+++ frontend/src/components/HighlightPopRemake/HighlightPop.ts
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react'
+import { HPopover, HPopoverItem } from './HighlightPopRemake.styled'
+
+
+interface HighlightPopProps {
+
+
+
+    children: ReactNode,
+}
+}
+
+const HighlightPop: React.FC<HighlightPopProps> = ({ }) => {
+    return (<div></div>)
+}
+export default HighlightPop
