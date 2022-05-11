efe20c2da02399fb06e3a63b726b5046df73b50f
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-05-11 20:55:35 +0200

    styling changes for DropdownFilter

--- frontend/src/components/FragmentsColumn/FilterWrapper/DropdownFilter/DropdownFilter.styled.js
+++ frontend/src/components/FragmentsColumn/FilterWrapper/DropdownFilter/DropdownFilter.styled.js
@@ -5,25 +5,28 @@ export const Main = styled.div`
   /* position: absolute; */
 `
 export const DropDownContainer = styled.div`
-  width: 12rem;
+  width: 8rem;
+  /* min-width: max-content; */
   min-width: min-content;
   margin: 0 auto;
 `
 
 export const DropDownHeader = styled.div`
   margin-bottom: 0.8em;
-  padding: 0.4rem 1rem 0.4rem 1rem;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  padding: 0.2rem 0.4rem 0.2rem 0.5rem;
+  /* box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15); */
   font-weight: 500;
-  font-size: 1.1rem;
-  color: var(--background4-main);
+  font-size: 0.9rem;
+  /* color: var(--background4-main); */
+  color: var(--background-secondary2);
   background: var(--background1-main);
   transition: all 0.3s ease-out;
   text-align: center;
+  border: 1px solid var(--background1-main);
   &:hover {
-    color: var(--background1-main);
-
-    background: var(--background4-main);
+    /* color: var(--background-secondary3); */
+    border: 1px solid var(--background-secondary2);
+    /* background: var(--background2-main); */
   }
   /* &:active {
     color: var(--background-secondary4);
@@ -36,14 +39,14 @@ export const DropDownListContainer = styled.div`
 export const DropDownList = styled.div`
   padding: 0;
   margin: 0;
-  padding-left: 1rem;
+  padding-left: 0.5rem;
   padding-right: 0.5rem;
-  background: var(--background2-main);
-  border: 2px solid var(--background5-main);
+  background: var(--background1-main);
+  border: 1px solid var(--background-secondary2);
   box-sizing: border-box;
-  color: var(--background4-main);
-  font-size: 1.1rem;
-  font-weight: 500;
+  color: var(--background-secondary2);
+  font-size: 1rem;
+  font-weight: 400;
   min-width: fit-content;
   &:first-child {
     padding-top: 0.8rem;
@@ -51,5 +54,5 @@ export const DropDownList = styled.div`
 `
 export const ListItem = styled.li`
   list-style: none;
-  margin-bottom: 0.8rem;
+  margin-bottom: 0.4rem;
 `
