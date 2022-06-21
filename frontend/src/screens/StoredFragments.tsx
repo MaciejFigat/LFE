eb39ebbe1f1269cd04d497f467c779014044f4f2
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-06-21 18:39:47 +0200

    styling and cleanup v3

--- frontend/src/screens/StoredFragments.tsx
+++ frontend/src/screens/StoredFragments.tsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react'
-import { useAppDispatch } from '../app/reduxHooks'
+import { useAppDispatch, useAppSelector } from '../app/reduxHooks'
+import { UserInfo } from '../interfaces'
 import DragAndDropMain from '../components/DragAndDrop/DragAndDropMain'
 import { getUserFragments } from '../features/fragments/fragmentSlice'
 interface StoredFragmentsProps {}
 
 const StoredFragments: React.FC<StoredFragmentsProps> = () => {
   const dispatch: any = useAppDispatch()
+  const userInfo: UserInfo = useAppSelector((state) => state.user.userInfo)
   useEffect(() => {
-    dispatch(getUserFragments(1))
-  }, [dispatch])
+    if (Object.keys(userInfo).length > 0) {
+      dispatch(getUserFragments(1))
+    }
+  }, [dispatch, userInfo])
   return <DragAndDropMain />
 }
 export default StoredFragments
