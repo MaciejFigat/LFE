9c5d71b71ed489745a4efa30ffc3d447fd15a849
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-09-03 23:34:22 +0200

    new HeroSection

--- frontend/src/components/InfoSection/HeroSection.styled.js
+++ frontend/src/components/InfoSection/HeroSection.styled.js
@@ -47,41 +47,60 @@ export const HeroSec = styled.div`
 export const HeroTextContainer = styled.div`
   z-index: 11;
   display: flex;
-  flex-direction: row;
+  /* flex-direction: row; */
+  flex-direction: column;
+  justify-content: space-around;
   /* background: var(--background-blur1); */
   /* margin-top: 1rem; */
   padding: 3rem;
   min-width: 70vw;
   /* max-width: 60vw; 
   */
+  svg {
+    position: static;
+  }
 `
 export const HeroTitle = styled.div`
-  width: 30%;
+  /* width: 30%; */
   font-weight: 700;
   font-size: 4.4375rem;
 
   color: var(--background5-main);
+  svg > defs:nth-child(2) {
+    /* stroke: var(--background3-main); */
+    stroke: red;
+    stop-color: red;
+  }
 `
 export const HeroStory = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  /* flex-direction: column; */
+  flex-direction: row;
+  /* align-items: flex-start; */
+  align-items: flex-end;
+  /* justify-content: flex-start; */
+  justify-content: flex-end;
+  /* border: 1px solid var(--background5-main); */
   /* display: grid;
   place-items: center; */
-  border-radius: 10px;
-  width: 70%;
+  /* border-radius: 10px; */
+  /* border-radius: 0px 20px 20px 20px; */
+  /* width: 50%; */
+  padding: 1rem;
   height: 300px;
-  font-weight: 500;
-  font-size: 26px;
+  font-weight: 700;
+  font-size: 2rem;
   color: var(--background5-main);
   /* background: var(--background-blur1); */
+  /* background: var(--background1-main); */
 `
 export const HeroP = styled.p`
   display: grid;
-  padding: 1rem;
+  /* padding: 1rem; */
   place-items: center;
+  /* border-radius: 0px 20px 20px 20px; */
   /* height: 3rem; */
-  border-radius: 5%;
-  background: var(--background-blur1);
+  /* border-radius: 5%; */
+  /* background: var(--background-blur1); */
+  /* background: var(--background1-main); */
 `
